56937a18696328278dd77ebc3f1ed2dddee32787
xyzgithome <2584118953@qq.com>
2024-01-02 21:48:18 +0800

    添加bean工厂后置处理器解析@Component @Bean @Mapper标注的bean

---
+++ tech-spring/src/main/java/com/tech/stack/spring/a05/mapper/Mapper1.java
@@ -0,0 +1,7 @@
+package com.tech.stack.spring.a05.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface Mapper1 {
+}
---
+++ tech-spring/src/main/java/com/tech/stack/spring/a05/mapper/Mapper3.java
@@ -0,0 +1,4 @@
+package com.tech.stack.spring.a05.mapper;
+
+public class Mapper3 {
+}
